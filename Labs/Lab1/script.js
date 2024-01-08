5603043a612386131de26dd638ffcc06fa613e1f
Cactus137 <anhdo13072004@gmail.com>
2024-01-08 16:48:59 +0700

    oce

---
+++ Labs/Lab1/script.js
@@ -0,0 +1,98 @@
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+// Lab1.1
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+// 4.
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
+// 5.
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+// 6.
+function printGoals(...player) {
+  console.log(player);
+  console.log(`${player.length} goals were scored`);
+}
+printGoals(...game.scored);
+// 7.
+const result = team1 || team2 ? "team1" : team2;
+console.log(result);
+team1 < team2 && console.log("team1 win");
+team1 > team2 && console.log("team2 win");
+
+// Lab1.2
+// 1.
+for (const [i, name] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${name}`);
+}
+// 2.
+let avg = 0;
+const odds = Object.values(game.odds);
+for (const odd of odds) {
+  avg += odd;
+}
+avg = avg / odds.length;
+console.log(`AVG = ${avg}`);
+// 3.
+for (const odd of Object.keys(game.odds)) {
+  let name = game[odd] || "Draw";
+  console.log(`Odd of ${name}: ${game.odds[odd]}`);
+}
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+//   console.log(`Odd of ${teamStr} ${odd}`);
+// }
+// 4.
+let scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
